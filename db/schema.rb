24c9050be2e63c9d11c3aa5a93226d86a37412ba
jareddickman <jdickman@stevens.edu>
2015-07-19 23:08:34 -0400

    hi

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716170624) do
+ActiveRecord::Schema.define(version: 20150720021600) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,12 +32,14 @@ ActiveRecord::Schema.define(version: 20150716170624) do
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
   create_table "ads", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.decimal  "price"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.integer  "variations"
     t.integer  "user_id"
     t.string   "description"
+    t.boolean  "paid"
+    t.text     "brief"
+    t.decimal  "price",       precision: 5, scale: 2
   end
 
   create_table "notes", force: :cascade do |t|
